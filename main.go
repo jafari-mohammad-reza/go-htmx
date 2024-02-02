d0211619ab540bb237a2adb776ac0139a613615b
Mohammadreza Jafari <mohammadrezajafari.dev@gmail.com>
2024-02-02 17:22:26 +0330

    add templ and echo setup

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/a-h/templ"
+	view "github.com/jafari-mohammad-reza/htmx/views"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello")
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	server := echo.New()
+	server.GET("/", func(c echo.Context) error {
+		return Render(c, view.Index("mohammad reza"))
+	})
+	server.Logger.Fatal(server.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
+}
+
+func Render(ctx echo.Context, comp templ.Component) error {
+	ctx.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+	return comp.Render(ctx.Request().Context(), ctx.Response().Writer)
 }
